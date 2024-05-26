d5703eb3ab19c4072e105b8907c07cbaa01b7572
Adriano F F <milicofelix@gmail.com>
2024-05-26 02:15:09 -0300

    feat: checkout no carrinho de compras

--- public/index.php
+++ public/index.php
@@ -5,18 +5,21 @@ require "../vendor/autoload.php";
 use Solid\CarrinhoCompras;
 
 $carrinho1 = new CarrinhoCompras();
-
+echo "<pre>";
 print_r($carrinho1->exibirItens());
+echo "<pre />";
 echo 'Valor total: '.$carrinho1->exibirValorTotal();
 
-/*
+
 $carrinho1->adicionarItem('Bicicleta', 750.10);
 $carrinho1->adicionarItem('Galadeira', 1950.15);
 $carrinho1->adicionarItem('Tapete', 350.20);
-*/
+
 
 echo "<br />";
+echo "<pre>";
 print_r($carrinho1->exibirItens());
+echo "<pre />";
 echo 'Valor total recalculado: '.$carrinho1->exibirValorTotal();
 
 echo "<br />";
