2aa32589016487220c74f410ecc93ca08487c463
Adriano F F <milicofelix@gmail.com>
2024-05-26 02:05:25 -0300

    chore: config and structure of the project

---
+++ public/index.php
@@ -0,0 +1,35 @@
+<?php 
+//var_dump(__DIR__);exit;
+require "../vendor/autoload.php";
+
+use Solid\CarrinhoCompras;
+
+$carrinho1 = new CarrinhoCompras();
+
+print_r($carrinho1->exibirItens());
+echo 'Valor total: '.$carrinho1->exibirValorTotal();
+
+/*
+$carrinho1->adicionarItem('Bicicleta', 750.10);
+$carrinho1->adicionarItem('Galadeira', 1950.15);
+$carrinho1->adicionarItem('Tapete', 350.20);
+*/
+
+echo "<br />";
+print_r($carrinho1->exibirItens());
+echo 'Valor total recalculado: '.$carrinho1->exibirValorTotal();
+
+echo "<br />";
+echo 'status: '.$carrinho1->exibirStatus();
+
+
+//$carrinho1->adicionarItem('Televisão 65"', 3570.25);
+
+echo "<br />";
+if($carrinho1->confirmarPedido()) {
+    echo 'Pedido realizado com sucesso!';
+} else {
+    echo 'Erro na confirmação do pedido. Carrinho não possui itens';
+}
+echo "<br />";
+echo 'status: '.$carrinho1->exibirStatus();
